aec686f2d7d92a4d6d7453e7a5b000fdabb9ea7a
crmviet <ng.manhtuong@gmail.com>
2021-05-02 14:43:02 +0700

    create node server

---
+++ backend/data.js
@@ -0,0 +1,79 @@
+const data = {
+    products: [
+       {
+        _id: '1', 
+        name: 'Nike Slim Shirt', 
+        image: '/images/p6.jpg', 
+        price: 120, 
+        brand: "Nike", 
+        countInStock: 5,
+        rating: 3.5, 
+        numReviews: 10, 
+        description: "high quality product"
+
+       },
+       {
+        _id: '2', 
+        name: 'Adidas Slim Shirt', 
+        image: '/images/p2.jpg', 
+        price: 100, 
+        countInStock: 10,
+        brand: "Adidas", 
+        rating: 2.5, 
+        numReviews: 10, 
+        description: "high adisas quality product"
+
+       },
+       {
+        _id: '3', 
+        name: 'Puma Slim Shirt', 
+        image: '/images/p3.jpg', 
+        price: 160, 
+        countInStock: 0,
+        brand: "puma", 
+        rating: 2.5, 
+        numReviews: 10, 
+        description: "high quality product"
+
+       },
+       {
+        _id: '4', 
+        name: 'Zara Slim Shirt', 
+        image: '/images/p4.jpg', 
+        price: 120, 
+        countInStock: 0,
+        brand: "Zara", 
+        rating: 4.0, 
+        numReviews: 8, 
+        description: "high zra quality product"
+
+       },
+       {
+        _id: '5', 
+        name: 'Uniqulo Slim Shirt', 
+        image: '/images/p5.jpg', 
+        price: 100, 
+        countInStock: 150,
+        brand: "Uniqlo", 
+        rating: 4, 
+        numReviews: 5.0, 
+        description: "high uniqlo quality product"
+
+       },
+       {
+        _id: '6', 
+        name: 'Vogue Slim Shirt', 
+        image: '/images/p6.jpg', 
+        price: 250, 
+        countInStock: 0,
+        brand: "Vogue", 
+        rating: 5.5, 
+        numReviews: 9, 
+        description: "high vogue quality product"
+
+       } 
+
+    ]
+}
+
+export default data;
