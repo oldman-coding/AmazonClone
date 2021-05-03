94650fb94dd3ff593f7837aca83fb4a206350984
crmviet <ng.manhtuong@gmail.com>
2021-05-03 16:00:00 +0700

    Add cart

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
+import { addToCart } from '../actions/cartAction';
 
 export default function CartScreen(props) {
-    const id = props.match.params.id;
+    const productId = props.match.params.id;
     const qty = props.location.search
                 ? Number(props.location.search.split("=")[1])
                 : 1 
+    const dispatch = useDispatch();
+    useEffect(() => {
+        if (productId) {
+            dispatch(addToCart(productId, qty))
+        }
+    }, [dispatch, productId, qty])
     return (
         <div>
             <h1>Cart Screen</h1>  
-            <p>ADD TO CART: PRODUCT_ID: {id} Qty: {qty}</p>
+            <p>ADD TO CART: PRODUCT_ID: {productId} Qty: {qty}</p>
         </div>
     )
 }
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -2,12 +2,20 @@ import {createStore, compose, applyMiddleware, combineReducers} from 'redux';
 import { productDetailsReducer, productListReducer } from './reducers/productReducers';
 import thunk from 'redux-thunk'; 
 import logger from 'redux-logger';
+import { cartReducer } from './reducers/cartReducer';
 
-const initialState = {};
+const initialState = {
+    cart: {
+        cartItems: localStorage.getItem('cartItems')
+        ? JSON.parse(localStorage.getItem('cartItems'))
+        : [], 
+    }
+};
+
 const reducer = combineReducers({
     productList: productListReducer, 
-    productDetails: productDetailsReducer
-
+    productDetails: productDetailsReducer, 
+    cart: cartReducer
 })
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
