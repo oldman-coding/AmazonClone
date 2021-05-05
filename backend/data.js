b2ca2e1843761fccfe276b033b4852293869325e
crmviet <ng.manhtuong@gmail.com>
2021-05-05 22:13:03 +0700

    Add user and Product to DB

--- backend/data.js
+++ backend/data.js
@@ -1,11 +1,27 @@
+import bcrypt from 'bcryptjs';
 const data = {
+    users: [
+        {
+            name: "Basir", 
+            email: "admin@example.com", 
+            password: bcrypt.hashSync('1234', 8), 
+            isAdmin: true,
+        }, 
+        {
+            name: "John", 
+            email: "user@example.com", 
+            password: bcrypt.hashSync('1234', 8), 
+            isAdmin: false,
+        }
+    ],
+
     products: [
        {
-        _id: '1', 
         name: 'Nike Slim Shirt', 
         image: '/images/p1.jpg', 
         price: 120, 
         brand: "Nike", 
+        category: "Shirt",
         countInStock: 5,
         rating: 3.5, 
         numReviews: 10, 
@@ -13,11 +29,11 @@ const data = {
 
        },
        {
-        _id: '2', 
         name: 'Adidas Slim Shirt', 
         image: '/images/p2.jpg', 
         price: 100, 
         countInStock: 10,
+        category: "Shirt",
         brand: "Adidas", 
         rating: 2.5, 
         numReviews: 10, 
@@ -25,11 +41,11 @@ const data = {
 
        },
        {
-        _id: '3', 
         name: 'Puma Slim Shirt', 
         image: '/images/p3.jpg', 
         price: 160, 
         countInStock: 0,
+        category: "Pants",
         brand: "puma", 
         rating: 2.5, 
         numReviews: 10, 
@@ -37,11 +53,11 @@ const data = {
 
        },
        {
-        _id: '4', 
         name: 'Zara Slim Shirt', 
         image: '/images/p4.jpg', 
         price: 120, 
         countInStock: 20,
+        category: "Pants",
         brand: "Zara", 
         rating: 4.0, 
         numReviews: 8, 
@@ -49,11 +65,11 @@ const data = {
 
        },
        {
-        _id: '5', 
         name: 'Uniqulo Slim Shirt', 
         image: '/images/p5.jpg', 
         price: 100, 
         countInStock: 150,
+        category: "Shoes",
         brand: "Uniqlo", 
         rating: 4, 
         numReviews: 5.0, 
@@ -61,11 +77,11 @@ const data = {
 
        },
        {
-        _id: '6', 
         name: 'Vogue Slim Shirt', 
         image: '/images/p6.jpg', 
         price: 250, 
         countInStock: 10,
+        category: "Shoes",
         brand: "Vogue", 
         rating: 5.5, 
         numReviews: 9, 
--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,29 @@
 import express from 'express';
-import data from './data.js';
+import mongoose from 'mongoose'; 
+import userRouter from './routers/userRouter.js';
+import productRouter from './routers/productRouter.js';
 
 const app = express();
 
-app.get('/api/products/:id', (req, res) => {
-  const product = data.products.find(x => x._id === req.params.id )
-  if (product) {
-    res.send(product);
-  }else {
-    res.status(404).send({message: 'Product not found'})
-  }
-})
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/api/products', (req, res) => {
-  res.send(data.products);
+mongoose.connect(process.env.MONGODB_URL || 'mongodb://localhost/amazonc', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
 });
+
+
+app.use('/api/users', userRouter); 
+app.use('/api/products', productRouter); 
 
 app.get('/', (req, res) => {
   res.send('Server is ready at port');
+});
+
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
 });
 
 const port = process.env.PORT || 5000;
