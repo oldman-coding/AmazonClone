79bbbd4364b95de245853cf38081c0c4ff2f0d0d
crmviet <ng.manhtuong@gmail.com>
2021-05-06 16:25:36 +0700

    Sign in backend

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -2,6 +2,8 @@ import express from 'express';
 import expressAsyncHandler from 'express-async-handler';
 import data from '../data.js';
 import User from '../models/userModel.js';
+import bcrypt from 'bcryptjs';
+import { generateToken } from '../ultils.js';
 
 const userRouter = express.Router();
 
@@ -13,5 +15,31 @@ userRouter.get(
     res.send({ createdUsers });
   })
 );
+
+userRouter.get('/', 
+  expressAsyncHandler(async (req, res) => {
+    // await User.remove({});
+    const users = await User.find({});
+    res.send({ users });
+}))
+
+userRouter.post('/signin', 
+  expressAsyncHandler(async(req, res) => {
+    const user = await User.findOne({email: req.body.email}); 
+    if (user) {
+      if (bcrypt.compareSync(req.body.password, user.password)) {
+        res.send({
+          _id: user._id, 
+          name: user.name, 
+          email: user.email, 
+          isAdmin: user.isAdmin,
+          token: generateToken(user)
+        }); 
+        return; 
+      }
+    }
+    else 
+      res.status(401).send({message: "Invalid email or password"});
+  }))
 
 export default userRouter;
--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,10 @@ import express from 'express';
 import mongoose from 'mongoose'; 
 import userRouter from './routers/userRouter.js';
 import productRouter from './routers/productRouter.js';
+import dotenv from 'dotenv';
 
+
+dotenv.config();
 const app = express();
 
 app.use(express.json());
