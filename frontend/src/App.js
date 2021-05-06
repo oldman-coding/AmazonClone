ed445407c4f4cf5d36121bda96d57bfa7fb5f6bd
crmviet <ng.manhtuong@gmail.com>
2021-05-06 17:03:57 +0700

    Sign in Screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Link, Route } from 'react-router-dom';
 import CartScreen from './screens/CartScreen';
 import HomeScreen from './screens/HomeScreen';
 import ProductScreen from './screens/ProductScreen';
+import SigninScreen from './screens/SigninScreen';
 
 function App() {
     const cart = useSelector(state => state.cart);
@@ -27,6 +28,7 @@ function App() {
             </header>
             <main>
                 <Route path='/cart/:id?' component ={CartScreen} />
+                <Route path='/signin' component = {SigninScreen} />
                 <Route path ='/product/:productId' component={ProductScreen} />
                 <Route exact path ='/' component = {HomeScreen} />
 
